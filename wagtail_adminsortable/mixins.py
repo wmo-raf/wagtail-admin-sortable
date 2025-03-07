d353657bd82c04617b5c5aba3e65bff6c504a54c
Erick Otenyo <otenyo.erick@gmail.com>
2025-03-07 10:13:48 +0300

    Replace deprecated is_ajax request method with external function

--- wagtail_adminsortable/mixins.py
+++ wagtail_adminsortable/mixins.py
@@ -2,6 +2,7 @@ import json
 
 from django.db import transaction
 from django.http import JsonResponse
+from .utils import is_ajax
 
 
 class AjaxableResponseMixin(object):
@@ -9,21 +10,22 @@ class AjaxableResponseMixin(object):
     Mixin to add AJAX support to a form.
     Must be used with an object-based FormView
     """
+    
     def form_invalid(self, form):
         response = super(AjaxableResponseMixin, self).form_invalid(form)
-        if self.request.is_ajax():
+        if is_ajax(self.request):
             return JsonResponse(form.errors, status=400)
         else:
             return response
-
+    
     def form_valid(self, form):
         super(AjaxableResponseMixin, self).form_valid(form)
-        if self.request.is_ajax():
+        if is_ajax(self.request):
             objects = json.loads(self.request.POST.get('objects', '[]'))
             data = {
                 'message': 'success'
             }
-
+            
             with transaction.atomic():
                 try:
                     for obj in objects:
@@ -31,5 +33,5 @@ class AjaxableResponseMixin(object):
                 except Exception as e:
                     str(e)
             return JsonResponse(data)
-
+        
         return JsonResponse({'message': 'Invalid Data'}, status=400)
